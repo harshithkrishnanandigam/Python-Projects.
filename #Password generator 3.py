ed8d9211c3548756b3fe5c2037d57c28d9f919f7
Nandigam Harshith Krishna <harshithkrishnanandigam@gmail.com>
2026-09-06 21:48:13 +0530

    Update #Password generator 3.py

--- #Password generator 3.py
+++ #Password generator 3.py
@@ -1,45 +1,69 @@
 #Password generator
+
 import random
-small_letters = 'abcdefghijklmnopqrstuvwxyz'
-capital_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-characters = '!@#$%^&*()_+=`~<>?:"}{|,./;[]`'
-numbers = '1234567890'
-sl = list(small_letters)
-cl = list(capital_letters)
-ch = list(characters)
-n = list(numbers)
 
-def password_generator():
-    try:
-        length = int(input('Enter the length of the password:'))
-    except ValueError:
-        print('Please enter a valid integer for the password length.') 
-    slist = []
-    clist = []
-    chlist = []
-    nlist = []
+print('============= PASSWORD GENERATOR ================')
+
+print('Password types available:')
+print('1. Small letters only')
+print('2. Capital letters only')
+print('3. Numbers only')
+print('4. Special characters only')
+print('5. Mixed (small, capital, numbers, special characters)')
+
+choice = input('Enter the number corresponding to your choice: ')
     
-    while length>=4:
-        schoosing= random.choice(sl)
-        cchoosing= random.choice(cl)
-        chchoosing= random.choice(ch)
-        nchoosing= random.choice(n)
-        length-=4
-        slist.append(schoosing)
-        clist.append(cchoosing)
-        chlist.append(chchoosing)
-        nlist.append(nchoosing)
-        password_list = slist+clist+nlist+chlist
-    while length>0:
-        f = random.choice(password_list)
-        password_list.append(f)
-        length-=1
-
-    random.shuffle(password_list)
-    b = ''.join(password_list)
-    return b
-
-print(password_generator())
-y = input('Do you want to save your password? (yes/no):')
-while y == 'yes':
-    password_generator()
+if choice == '1':
+        x = int(input('Enter your password length:'))
+        def password_generator(x):
+            small_letters = 'abcdefghijklmnopqrstuvwxyz'
+            p = []
+            for i in range(x):
+                pas = random.choice(small_letters)
+                p.append(pas)
+            return ''.join(p)
+        print(password_generator(x))
+    
+elif choice == '2':
+        x = int(input('Enter your password length:'))
+        def password_generator(x):
+            capital_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+            p = []
+            for i in range(x):
+                pas = random.choice(capital_letters)
+                p.append(pas)
+            return ''.join(p)
+        print(password_generator(x))
+        
+elif choice == '3':
+        x = int(input('Enter your password length:'))
+        def password_generator(x):
+            numbers = '1234567890'
+            p = []
+            for i in range(x):
+                pas = random.choice(numbers)
+                p.append(pas)
+            return ''.join(p)
+        print(password_generator(x))
+        
+elif choice == '4':
+        x = int(input('Enter your password length:'))
+        def password_generator(x):
+            special_characters = '!@#$%^&*()_+=`~<>?:"}{|,./;[]`'
+            p = []
+            for i in range(x):
+                pas = random.choice(special_characters)
+                p.append(pas)
+            return ''.join(p)
+        print(password_generator(x))
+        
+elif choice == '5':
+        x = int(input('Enter your password length:'))
+        def password_generator(x):
+            mixed_characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+=`~<>?:"}{|,./;[]`'
+            p = []
+            for i in range(x):
+                pas = random.choice(mixed_characters)
+                p.append(pas)
+            return ''.join(p)
+        print(password_generator(x))
